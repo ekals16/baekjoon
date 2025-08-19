a889ad3dca8457493f5aaa403bba4eef94697110
ekals16 <jdamin0106@gmail.com>
2025-08-19 16:13:49 +0900

    백준 10797번 문제 업로드

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-year = int(input())
+day = int(input())
+c1,c2,c3,c4,c5 = map(int, input().split())
+n=0
 
-if (year%4==0) and ( year%100!=0 or year%400==0 ):
-    print(1)
-else:
-    print(0)
+if day==c1:
+    n = n+1
+if day==c2:
+    n = n+1
+if day==c3:
+    n = n+1
+if day==c4:
+    n = n+1
+if day==c5:
+    n = n+1
+
+print(n)
