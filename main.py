04b794d5a50866281c9d9695687695434452c1ab
ekals16 <jdamin0106@gmail.com>
2025-08-19 16:53:48 +0900

    백준 2525번 문제 풀이 업로드

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-day = int(input())
-c1,c2,c3,c4,c5 = map(int, input().split())
-n=0
+A,B = map(int, input().split())
+C = int(input())
+total_minute = A*60 + B + C  
 
-if day==c1:
-    n = n+1
-if day==c2:
-    n = n+1
-if day==c3:
-    n = n+1
-if day==c4:
-    n = n+1
-if day==c5:
-    n = n+1
-
-print(n)
+print((total_minute//60)%24, total_minute%60)
