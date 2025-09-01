b8f55c87220670b789c60c26040581b3909a8c06
ekals16 <jdamin0106@gmail.com>
2025-09-01 15:18:46 +0900

    백준 10101 문제 풀이 업로드

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-A,B = map(int, input().split())
-C = int(input())
-total_minute = A*60 + B + C  
+a1 = int(input())
+a2 = int(input())
+a3 = int(input()) 
 
-print((total_minute//60)%24, total_minute%60)
+if a1 == a2 == a3 == 60:
+    print("Equilateral")
+elif a1+a2+a3 == 180 and (a1==a2 or a2==a3 or a3==a1):
+    print("Isosceles")
+elif a1+a2+a3 == 180:
+    print("Scalene")
+else:
+    print("Error")
